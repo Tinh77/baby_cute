1f79c76f3ba61ce63e5b25c24fac256c5b8d986b
ThanhTung1235 <tungltd00602@fpt.edu.vn>
2018-08-14 12:30:55 +0700

    Home page demo

---
+++ resources/views/masterLayout/content/login.blade.php
@@ -0,0 +1,24 @@
+<!-- Material form subscription -->
+<div class="card">
+
+    <h5 class="card-header info-color white-text text-center py-4">
+        <strong>Subscribe</strong>
+    </h5>
+    <div class="card-body px-lg-5">
+        <form class="text-center" style="color: #757575;">
+            <p>Join our mailing list. We write rarely, but only the best content.</p>
+            <p>
+                <a href="" target="_blank">See the last newsletter</a>
+            </p>
+            <div class="md-form mt-3">
+                <input type="text" id="materialSubscriptionFormPasswords" class="form-control">
+                <label for="materialSubscriptionFormPasswords">Name</label>
+            </div>
+            <div class="md-form">
+                <input type="email" id="materialSubscriptionFormEmail" class="form-control">
+                <label for="materialSubscriptionFormEmail">E-mail</label>
+            </div>
+            <button class="btn btn-outline-info btn-rounded btn-block z-depth-0 my-4 waves-effect" type="submit">Sign in</button>
+        </form>
+    </div>
+</div>
